88755d2a6431a681feb9215087eed0a95c793a0f
kapilneha <nehakapil456@gmail.com>
2026-07-27 13:58:32 +0530

    contains duplicate

---
+++ contains_duplicate.py
@@ -0,0 +1,17 @@
+def containsDuplicate(nums):
+
+    seen = set()
+
+    for num in nums:
+
+        if num in seen:
+            return True
+
+        seen.add(num)
+
+    return False
+
+
+nums = list(map(int, input("Enter numbers: ").split()))
+
+print(containsDuplicate(nums))
